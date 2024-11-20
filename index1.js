b751f0442c7d80419b445aa0146e66744fd76979
RajeshKumarG <rajesh.kumar@techpearl.com>
2024-11-20 19:15:33 +0530

    added js file

---
+++ index1.js
@@ -0,0 +1,34 @@
+// variable = A container that stores a value.
+//            Behaves as if it were the values it contains.
+
+// 1. declaration   let x;
+// 2. assignment    x = 100;
+
+// let age = 25;
+// let price = 10.99;
+// let gpa = 2.1;
+
+// let firstname = "rajeshkumarg_18";
+// let favouriteFood = "pizza";
+// let email = "fab249719@gmail.com";
+
+let offline = true;
+
+let online = false;
+let forSale = true;
+let isStudent = true;
+
+console.log(`typeof online`);
+console.log(`rajeshkumarg_18 is online: ${online}`);
+console.log(`Is this car for sale: ${forSale}`);
+console.log(`Enrolled: ${isStudent}`);
+
+
+console.log(typeof firstname);
+console.log(`Your name is ${fullName}`);
+console.log(`You like ${favouriteFood}`);
+console.log(`Your email is ${email}`);
+console.log(typeof price);
+console.log(`You are ${age1} years old`);
+console.log(`The price is $${price}`);
+console.log(`Your gpa is: ${gpa}`);
