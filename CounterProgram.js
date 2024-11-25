fcdb9522cb0e3a76c3f86729bc0c6b44c64e8344
RajeshKumarG <rajesh.kumar@techpearl.com>
2024-11-25 17:54:00 +0530

    Added MathObject program

--- CounterProgram.js
+++ CounterProgram.js
@@ -3,19 +3,24 @@
 const decreaseBtn = document.getElementById("decreaseBtn");
 const resetBtn = document.getElementById("resetBtn");
 const increaseBtn = document.getElementById("increaseBtn");
-const countLabe1 = document.getElementById("countLabe1");
+const countLabel1 = document.getElementById("countLabel1");
+const countLabel2 = document.getElementById("countLabel2");
+
 let count = 0;
 
 increaseBtn.onclick = function(){
     count++;
-    countLabe1.textContent = count;
+    countLabel1.textContent = count;
+    countLabel2.textContent = count - 10;
 }
 decreaseBtn.onclick = function(){
     count--;
-    countLabe1.textContent = count;
+    countLabel1.textContent = count;
+    countLabel2.textContent = count -10;
 }
 resetBtn.onclick = function(){
     count = 0;
-    countLabe1.textContent = count;
+    countLabel1.textContent = count;
+    countLabel2.textContent = count -10;
 }
 
