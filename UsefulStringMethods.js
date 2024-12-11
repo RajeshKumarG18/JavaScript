655e0ac734eb7d0a96e83fba330ab033bbb3cceb
RajeshKumarG18 <rajesh.kumar@techpearl.com>
2024-12-11 17:57:36 +0530

    Added UsefulStringMethods.js
    Added UsefulStringMethods.js

---
+++ UsefulStringMethods.js
@@ -0,0 +1,16 @@
+// useful string properties and methods
+
+let userName = "rajeshkumar.g18"
+let phoneNumber = "797-504-4147"
+
+//console.log(userName.length);
+//console.log(userName.charAt(0));
+//console.log(userName.indexOf("o"));
+//console.log(userName.lastIndexOf("o"));
+//userName = userName.trim();
+//userName = userName.toUpperCase();
+//userName = userName.toLowerCase();
+
+phoneNumber = phoneNumber.replaceAll("-","")
+
+console.log(phoneNumber);
